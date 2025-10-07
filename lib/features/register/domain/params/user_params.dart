d664ef12bbadadf72cfa6cea8dcba4cca042ea04
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2025-10-08 01:11:59 +0300

    create class user params in domain layer

---
+++ lib/features/register/domain/params/user_params.dart
@@ -0,0 +1,12 @@
+
+class UserParams {
+  final String username;
+  final String email;
+  final String password;
+
+  UserParams({
+    required this.username,
+    required this.email,
+    required this.password,
+  });
+}
