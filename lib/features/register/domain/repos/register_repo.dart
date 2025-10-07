646f1c24817ffaf6d8d603e7e0396afe28a39ed7
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2025-10-08 01:12:49 +0300

    recostruct register repo in domain layer

--- lib/features/register/domain/repos/register_repo.dart
+++ lib/features/register/domain/repos/register_repo.dart
@@ -1,11 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:register_page/core/errors/failure.dart';
 import 'package:register_page/features/register/domain/entities/user_entity.dart';
+import 'package:register_page/features/register/domain/params/user_params.dart';
 
 abstract class RegisterRepo {
-  Future<Either<Failure, UserEntity>> register(
-    String username,
-    String email,
-    String password,
-  );
+  Future<Either<Failure, UserEntity>> register(UserParams params);
 }
